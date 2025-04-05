2763eb7d112b67e2b9c526ea1fca63d2e4a0d0b3
cythreal <cythreal@gmail.com>
2025-04-05 12:07:09 -0400

    Manage update + draw with groups, finish ship movements

--- main.py
+++ main.py
@@ -9,18 +9,27 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     FPS = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
+    
     while screen:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for model in drawable:
+            model.draw(screen)
         pygame.display.flip()
         
-
         dt = FPS.tick(60) / 1000 #pause game loop until 1/60 of second passes,
         #AND return value in milliseconds to dt
+
 if __name__ == "__main__":
     main()
