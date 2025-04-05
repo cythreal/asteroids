3cea71046f1a19e7ad2ff188df76fd620e588384
cythreal <cythreal@gmail.com>
2025-04-05 11:25:37 -0400

    FPS defined, sprite classes created, player draws to screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,11 +8,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    FPS = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while screen:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+
+        dt = FPS.tick(60) / 1000 #pause game loop until 1/60 of second passes,
+        #AND return value in milliseconds to dt
 if __name__ == "__main__":
     main()
